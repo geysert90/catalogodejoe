a12589a7c900df6dcb510564fd78ff9f6abd1213
darkness <darkness@darknesss-MacBook-Pro.local>
2025-08-31 03:09:10 +0200

    vista mobil3

--- components/slider/Hero1Slider.js
+++ components/slider/Hero1Slider.js
@@ -183,7 +183,7 @@ export default function Hero1Slider({ banners = [] }) {
         }
         @media (max-width: 767.98px) {
           .hero-slide {
-            min-height: 58vh;
+            min-height: 62vh;
           }
         }
 
